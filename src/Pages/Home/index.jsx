f1da517a1f6dc87eba8ba168ff80067a9d6e78eb
ca r213.06 <ca.r213.06@ca.local>
2023-01-24 13:37:21 +0400

    hometask

---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Section1 from '../../Components/Section1'
+import Navbar from '../../Layouts/Navbar'
+
+function HomePage() {
+  return (
+    <>
+    <Navbar></Navbar>
+    <Section1></Section1>
+    </>
+  )
+}
+
+export default HomePage
